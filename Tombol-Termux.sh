c3888948c9875fb50f1585b0ab3c077b2f6f908c
KamZ0 <74688013+KamZ0@users.noreply.github.com>
2020-11-21 05:37:54 +0700

    Update Tombol-Termux.sh

--- Tombol-Termux.sh
+++ Tombol-Termux.sh
@@ -4,7 +4,7 @@ echo "Tools Install Tombol Panah"
 sleep 2
 echo "=============================" 
 sleep 3
-echo "Sedang Menginstall Bahan" 
+echo    "Sedang Menginstall Bahan" 
 sleep 4
 echo "============================="
 sleep 5
@@ -14,10 +14,10 @@ pkg install python -y
 pkg install python2 -y
 pip2 install bkey
 bkey
+bkey
 
 echo "Penginstalan Selesai"
 sleep 6
 echo "Silakan Close Termux"
 sleep 7
 echo "Tekan Ctrl+D Lalu Tekan Enter"
-sleep 8
